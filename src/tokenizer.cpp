2f04083eb983a21b3c98ced6e6ad429fe44ee736
Salvak <6888784+Salvakiya@users.noreply.github.com>
2024-12-26 15:58:01 -0600

    add cmakelists and setup main cpp file

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -178,24 +178,3 @@ public:
         return token;
     }
 };
-
-int main() {
-    std::string CODE = R"(
-//this is a comment
-let x = 10;
-let y = 20; // another comment
-let /*a = /*14*/;
-let*/ b = 26;
-mut(a) += x;
-print(x+y);
-)";
-
-    Tokenizer tokenizer(CODE);
-    while (true) {
-        Token tok = tokenizer.next_token();
-        if (tok.type == TokenType::EOF_TOKEN) break;
-        std::cout << "line " << tok.line_number << ", position " << tok.line_position << ": " << getTokenName(tok.type) << ": " << tok.value 
-                  << std::endl;
-    }
-    return 0;
-}
