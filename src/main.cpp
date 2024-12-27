eafc71667bfca756b0913cf98f54ae0cfea432ec
Salvak <6888784+Salvakiya@users.noreply.github.com>
2024-12-27 00:10:09 -0600

    comment out token peek example

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main() {
     std::string CODE = R"(
 //this is a comment
 let x = 10.5;
+let name = "some name HERE 'with other' else";
+let name2 = 'some name HERE "with other" else';
 let y = 20; // another comment
 let /*a = /*14*/;
 let*/ b = 26;
@@ -19,9 +21,9 @@ print(x+y);
         if (tok.type == TokenType::EOF_TOKEN) break;
         std::cout << "line " << tok.line_number << ", position " << tok.line_position << ": " << getTokenName(tok.type) << ": " << tok.value 
                   << std::endl;
-        tok = tokenizer.peek_token(5);
-        std::cout << "line " << tok.line_number << ", position " << tok.line_position << ": " << getTokenName(tok.type) << ": " << tok.value 
-                  << std::endl;
+        // tok = tokenizer.peek_token(5);
+        // std::cout << "line " << tok.line_number << ", position " << tok.line_position << ": " << getTokenName(tok.type) << ": " << tok.value 
+        //           << std::endl;
     }
     return 0;
 }
